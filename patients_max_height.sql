ed1fd63a98166126533e3b97dccabd7409684a51
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-09-25 16:44:41 +0530

    Update patients_max_height.sql
    Show the first_name, last_name, and height of the patient with the greatest height.

--- patients_max_height.sql
+++ patients_max_height.sql
@@ -1,3 +1,9 @@
-SELECT height
+SELECT first_name,last_name,height
 From patients
 where height = (select MAX(height)FROM patients);
+----------------------------------------------------
+SELECT
+  first_name,
+  last_name,
+  MAX(height) AS height
+FROM patients;
