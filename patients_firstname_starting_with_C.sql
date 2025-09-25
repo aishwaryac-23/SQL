f87361f4c702cddfd6d0c2d71d9268e31282fd1b
aishwaryac-23 <144509464+aishwaryac-23@users.noreply.github.com>
2025-09-25 15:56:52 +0530

    Update patients_firstname_starting_with_C.sql

--- patients_firstname_starting_with_C.sql
+++ patients_firstname_starting_with_C.sql
@@ -1,3 +1,7 @@
 SELECT first_name
 FROM patients
 WHERE first_name like  'C%'
+--------------------------------------------
+SELECT first_name
+FROM patients
+WHERE substring(first_name, 1, 1) = 'C'
